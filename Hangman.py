a21244732adb5e9790c316cddb2d7a98595f0737
Carlo Di Paolo <carlo.dipaolo94@gmail.com>
2025-07-27 22:31:50 +0200

    def Display Board

--- Hangman.py
+++ Hangman.py
@@ -41,3 +41,22 @@ def getRandomWord(wordList):
     #return a random string from the word list
     wordIndex=random.randing(0, len(wordList)-1)
     return wordList[wordIndex]
+
+def displayBoard(missedLetters, correctLetters, secretWord):
+    print(HANGMAN_PICS[len(missedLetters)])
+    print()
+
+    print('Missed letters:', end=' ')
+    for letter in missedLetters:
+        print(letter, end=' ')
+    print()
+
+    blanks = '_' * len(secretWord)
+
+    for i in range(len(secretWord)): 
+        if secretWord[i] in correctLetters:
+            blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+
+    for letter in blanks: 
+        print(letter, end=' ')
+    print()
